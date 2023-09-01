3fdd0a668d40d9de1c4cb38dac0c539e2b1f4d96
shivamsinghcse <shivamsinghcse@gmail.com>
2023-09-01 18:23:40 +0530

    Added index page

---
+++ app/components/writting.tsx
@@ -0,0 +1,87 @@
+import React from 'react'
+
+export function Writting() {
+  return (
+    <>
+    <div className="mx-auto grid w-full max-w-7xl items-center space-y-4 px-2 py-10 md:grid-cols-2 md:gap-6 md:space-y-0 lg:grid-cols-4">
+  <div className="relative aspect-[16/9]  w-auto rounded-md md:aspect-auto md:h-[400px]">
+    <img
+      src="https://images.unsplash.com/photo-1588099768531-a72d4a198538?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8NnwxMTM4MTU1NXx8ZW58MHx8fHw%3D&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
+      alt="AirMax Pro"
+      className="z-0 h-full w-full rounded-md object-cover"
+    />
+    <div className="absolute inset-0 rounded-md bg-gradient-to-t from-gray-900 to-transparent"></div>
+    <div className="absolute bottom-4 left-4 text-left">
+      <h1 className="text-lg font-semibold text-white">Nike Airmax v2</h1>
+       
+      <button className="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-white">
+        Shop Now →
+      </button>
+    </div>
+  </div>
+  <div className="relative aspect-[16/9]  w-auto rounded-md md:aspect-auto md:h-[400px]">
+    <img
+      src="https://images.unsplash.com/photo-1588099768531-a72d4a198538?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8NnwxMTM4MTU1NXx8ZW58MHx8fHw%3D&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
+      alt="AirMax Pro"
+      className="z-0 h-full w-full rounded-md object-cover"
+    />
+    <div className="absolute inset-0 rounded-md bg-gradient-to-t from-gray-900 to-transparent"></div>
+    <div className="absolute bottom-4 left-4 text-left">
+      <h1 className="text-lg font-semibold text-white">Nike Airmax v2</h1>
+       
+      <button className="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-white">
+        Shop Now →
+      </button>
+    </div>
+  </div>
+  <div className="relative aspect-[16/9]  w-auto rounded-md md:aspect-auto md:h-[400px]">
+    <img
+      src="https://images.unsplash.com/photo-1588099768531-a72d4a198538?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8NnwxMTM4MTU1NXx8ZW58MHx8fHw%3D&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
+      alt="AirMax Pro"
+      className="z-0 h-full w-full rounded-md object-cover"
+    />
+    <div className="absolute inset-0 rounded-md bg-gradient-to-t from-gray-900 to-transparent"></div>
+    <div className="absolute bottom-4 left-4 text-left">
+      <h1 className="text-lg font-semibold text-white">Nike Airmax v2</h1>
+       
+      <button className="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-white">
+        Shop Now →
+      </button>
+    </div>
+  </div>
+  <div className="relative aspect-[16/9]  w-auto rounded-md md:aspect-auto md:h-[400px]">
+    <img
+      src="https://images.unsplash.com/photo-1588099768531-a72d4a198538?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8NnwxMTM4MTU1NXx8ZW58MHx8fHw%3D&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
+      alt="AirMax Pro"
+      className="z-0 h-full w-full rounded-md object-cover"
+    />
+    <div className="absolute inset-0 rounded-md bg-gradient-to-t from-gray-900 to-transparent"></div>
+    <div className="absolute bottom-4 left-4 text-left">
+      <h1 className="text-lg font-semibold text-white">Nike Airmax v2</h1>
+       
+      <button className="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-white">
+        Shop Now →
+      </button>
+    </div>
+  </div>
+{/* </div> */}
+<div className="relative aspect-[16/9]  w-auto rounded-md md:aspect-auto md:h-[400px]">
+    <img
+      src="https://images.unsplash.com/photo-1588099768531-a72d4a198538?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8NnwxMTM4MTU1NXx8ZW58MHx8fHw%3D&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
+      alt="AirMax Pro"
+      className="z-0 h-full w-full rounded-md object-cover"
+    />
+    <div className="absolute inset-0 rounded-md bg-gradient-to-t from-gray-900 to-transparent"></div>
+    <div className="absolute bottom-4 left-4 text-left">
+      <h1 className="text-lg font-semibold text-white">Nike Airmax v2</h1>
+       
+      <button className="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-white">
+        Shop Now →
+      </button>
+    </div>
+  </div>
+</div>
+
+    </>
+  )
+}
