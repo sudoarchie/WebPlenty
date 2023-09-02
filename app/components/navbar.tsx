96833eb833c0c78fc4c20d893d5d4033c537f765
shivamsinghcse <shivamsinghcse@gmail.com>
2023-09-02 21:36:06 +0530

    Adding route to login

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import React from 'react'
+import Link from 'next/link'
 import { Menu, X } from 'lucide-react'
 
 
@@ -69,7 +70,7 @@ export function Navbar() {
             type="button"
             className="rounded-md bg-orange-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-orange-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
           >
-            Login
+             <Link href="/login">Login</Link>
           </button>
         </div>
         <div className="lg:hidden">
