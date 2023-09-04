9dadfede0bbf81bc1f24be9f1d1e1f6eefc69352
shivamsinghcse <shivamsinghcse@gmail.com>
2023-09-04 18:33:30 +0530

    Adding Login route for small screen devices

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -128,7 +128,7 @@ export function Navbar() {
                   type="button"
                   className="mt-4 w-full rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
                 >
-                  Login
+                  <Link href="/login">Login</Link>
                 </button>
               </div>
             </div>
