eef540916ed6300ad8703e07d47d19763bfbf66f
krishnoit <krishnoit100@gmail.com>
2023-09-29 08:36:17 +0530

    new

--- app/karykarta/page.tsx
+++ app/karykarta/page.tsx
@@ -1,27 +1,64 @@
-import React from 'react'
-// import { Navbar } from '../components/navbar'
-import { NavbarLogout } from '../components/navbarlogout'
-import { TableData } from '../components/table'
-import { Sidebar } from '../components/sidebar'
+'use client'
+import React, { useState, useEffect } from "react";
+import { NavbarLogout } from "../components/navbarlogout";
+import { TableData } from "./table";
+import { Sidebar } from "../components/sidebar";
+import axios from "axios";
+import { api, baseURL } from "../pages/api";
 
-function page() {
+const Page = () => {
+  const [data, setData] = useState();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const fetchData = async () => {
+    try {
+      const response = await api.get('/karykarta');
+      console.log(response.data.data)
+      setData(response.data.data);
+      setLoading(false);
+    } catch (error:any) {
+      setError(error);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <>
-    <div className='w-[100vw] fixed z-10'>
-    <NavbarLogout></NavbarLogout>  
-    </div>
-    <div className='flex'>
-      <div className='fixed'>
-
-    <Sidebar></Sidebar>
+      <div className="w-[100vw] fixed z-10">
+        <NavbarLogout />
       </div>
-    <div className='w-[80vw] relative top-10  left-64'>
-    <div className='flex justify-center'><h1 className='text-2xl font-extrabold mt-10 '>केतकी सिंह विधायक 362 विधान सभा बांसडीह</h1></div>
-    <TableData></TableData>
-    </div>
-    </div>
+      <div className="flex">
+        <div className="fixed">
+          <Sidebar />
+        </div>
+        <div className="w-[80vw] relative top-10 left-64">
+          {loading ? (
+            <div className="text-center">
+              <p>Loading...</p>
+            </div>
+          ) : error ? (
+            <div className="text-center">
+              <p>Error: {error}</p>
+            </div>
+          ) : (
+            <>
+              <div className="flex justify-center">
+                <h1 className="text-2xl font-extrabold mt-10">
+                  केतकी सिंह विधायक 362 विधान सभा बांसडीह
+                </h1>
+              </div>
+              <TableData data={data} />
+            </>
+          )}
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default page
+export default Page;
--- app/karykarta/table.tsx
+++ app/karykarta/table.tsx
@@ -1,72 +1,9 @@
 import React from 'react'
 import { ArrowLeft, ArrowRight } from 'lucide-react'
 
-const data = [
-  {
-    department: 'Engineering',
-    employees: [
-      {
-        id: 1,
-        name: 'John Doe',
-        address: '123 Main St',
-        mobileNumber: '555-1234',
-        dob: '1990-01-01',
-        religion: 'Hindu',
-        gender: 'Male',
-        previousParty: 'None',
-        mundalId: 1,
-        sectorId: 2,
-        poolingBoothId: 3,
-        role: 'कार्यकर्ता',
-      },
-      {
-        id: 2,
-        name: 'John Doe',
-        address: '123 Main St',
-        mobileNumber: '555-1234',
-        dob: '1990-01-01',
-        religion: 'Hindu',
-        gender: 'Male',
-        previousParty: 'None',
-        mundalId: 1,
-        sectorId: 2,
-        poolingBoothId: 3,
-        role: 'कार्यकर्ता',
-      },
-    ],
-  },
-  {
-    department: 'Marketing',
-    employees: [
-      {
-        id: 1,
-        name: 'Seb Cook',
-        title: 'Social Media',
-        department: 'Marketing',
-        email: 'seb@devui.com',
-        role: 'Social Media Manager',
-        image:
-          'https://images.unsplash.com/photo-1557862921-37829c790f19?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1742&q=80',
-      },
-      {
-        id: 2,
-        name: 'John Doe',
-        address: '123 Main St',
-        mobileNumber: '555-1234',
-        dob: '1990-01-01',
-        religion: 'Hindu',
-        gender: 'Male',
-        previousParty: 'None',
-        mundalId: 1,
-        sectorId: 2,
-        poolingBoothId: 3,
-        role: 'कार्यकर्ता',
-      },
-    ],
-  },
-];
 
-export function TableData() {
+export function TableData({data}:any) {
+ 
   return (
     <>
 <table className="min-w-full divide-y divide-gray-200">
@@ -100,19 +37,29 @@ export function TableData() {
             Mundal ID
           </th>
           <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-            Sector ID
-          </th>
-          <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-            Pooling Booth ID
-          </th>
-          <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
             Role
           </th>
         </tr>
       </thead>
       <tbody className="bg-white divide-y divide-gray-200">
-      {data.map((department) =>
-            department.employees.map((person) => (
+        {
+          data.map((info:any)=>(
+            <tr key={info.id}>
+            <td className="px-6 py-4 whitespace-nowrap text-xs">{info.id}</td>
+            <td className="px-6 py-4 whitespace-nowrap text-xs">{info.name}</td>
+            <td className="px-6 py-4 whitespace-nowrap text-xs">{info.address}</td>
+            <td className="px-6 py-4 whitespace-nowrap text-xs">{info.mobileNumber}</td>
+            <td className="px-6 py-4 whitespace-nowrap text-xs">{info.dob}</td>
+            <td className="px-6 py-4 whitespace-nowrap text-xs">{info.religion}</td>
+            <td className="px-6 py-4 whitespace-nowrap text-xs">{info.gender}</td>
+            <td className="px-6 py-4 whitespace-nowrap text-xs">{info.previousParty}</td>
+            <td className="px-6 py-4 whitespace-nowrap text-xs">{info.mundalId}</td>
+            <td className="px-6 py-4 whitespace-nowrap text-xs">{info.role}</td>
+          </tr>
+          ))
+        }
+      {/* {data?data.map((info:any) =>
+            department.employees.map((person:any) => (
           <tr key={person.id}>
             <td className="px-6 py-4 whitespace-nowrap">{person.id}</td>
             <td className="px-6 py-4 whitespace-nowrap">{person.name}</td>
@@ -127,7 +74,7 @@ export function TableData() {
             <td className="px-6 py-4 whitespace-nowrap">{person.poolingBoothId}</td>
             <td className="px-6 py-4 whitespace-nowrap">{person.role}</td>
           </tr>
-        )))}
+        ))):""} */}
       </tbody>
     </table>
     </>
