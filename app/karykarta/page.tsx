bad90ccc5251af514cfc1a9f5098e8167b56727c
krishnoit <krishnoit100@gmail.com>
2023-09-29 13:06:00 +0530

    ✨ New feature new form refcator code

--- app/karykarta/page.tsx
+++ app/karykarta/page.tsx
@@ -5,12 +5,12 @@ import { TableData } from "./table";
 import { Sidebar } from "../components/sidebar";
 import axios from "axios";
 import { api, baseURL } from "../pages/api";
-import { ArrowLeft } from 'lucide-react'
+import { ArrowLeft } from "lucide-react";
 
 const Page = () => {
   const [data, setData] = useState();
   const [loading, setLoading] = useState(true);
-  const [infoError, setInfoError] = useState(null); // Corrected variable name
+  const [infoError, setInfoError] = useState(null);
 
   const fetchData = async () => {
     try {
@@ -19,11 +19,10 @@ const Page = () => {
       setData(response.data.data);
       setLoading(false);
     } catch (error: any) {
-      // Removed ":any" type annotation
       console.log(error);
-      setInfoError(error.code); // Corrected function name
+      setInfoError(error.code);
       setLoading(false);
-      console.log(infoError); // You can remove this line as it may not show the updated state immediately
+      console.log(infoError);
     }
   };
 
@@ -33,30 +32,37 @@ const Page = () => {
   console.log(infoError);
   return (
     <>
-    <div className="w-[100vw] fixed z-10">
-    <NavbarLogout />
+      <div className="w-[100vw] fixed z-10">
+        <NavbarLogout />
       </div>
-    <div className="flex">
+      <div className="flex">
         <div className="fixed">
           <Sidebar />
         </div>
         <div className="w-[80vw] relative top-10 left-64">
           {loading ? (
-    <div className="text-center">
+            <div className="text-center">
               <p>Loading...</p>
             </div>
-          ) : infoError ? ( // Corrected variable name
-          <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-          <div className="text-center">
-            <p className="text-base font-semibold text-black">404</p>
-            <h1 className="mt-2 text-3xl font-bold tracking-tight text-black sm:text-5xl">
-             Data cannot load !!!!
-            </h1>
-            <p className="mt-4 text-base leading-7 text-gray-600">
-              Sorry, we could not fetch data at this moment
-            </p>
-          </div>
-        </div>
+          ) : infoError ? (
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "100vh",
+              }}
+            >
+              <div className="text-center">
+                <p className="text-base font-semibold text-black">404</p>
+                <h1 className="mt-2 text-3xl font-bold tracking-tight text-black sm:text-5xl">
+                  Data cannot load !!!!
+                </h1>
+                <p className="mt-4 text-base leading-7 text-gray-600">
+                  Sorry, we could not fetch data at this moment
+                </p>
+              </div>
+            </div>
           ) : (
             <>
               <div className="flex justify-center">
@@ -64,6 +70,49 @@ const Page = () => {
                   केतकी सिंह विधायक 362 विधान सभा बांसडीह
                 </h1>
               </div>
+              <tr className="border-2 border-gray-500">
+                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"></th>
+                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"></th>
+                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"></th>
+                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"></th>
+                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  <select
+                    name="Religion"
+                    id="Religion"
+                    className="w-full p-2 mb-4 border rounded-md"
+                  >
+                    <option value="hindu">Hindu</option>
+                    <option value="muslim">Muslim</option>
+                    <option value="christian">Christian</option>
+                    <option value="christian">Skih</option>
+                    <option value="christian">Jain</option>
+                    <option value="christian">Jews</option>
+                    <option value="other">Other</option>
+                  </select>
+                </th>
+                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  <select className="w-full p-2 mb-4 border rounded-md">
+                    <option value="hindu">Male</option>
+                    <option value="muslim">Female</option>
+                    <option value="muslim">Other</option>
+                  </select>
+                </th>
+                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Previous Party
+                </th>
+                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Mundal ID
+                </th>
+                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Role
+                </th>
+                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Edit
+                </th>
+                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Delete
+                </th>
+              </tr>
               <TableData data={data} />
             </>
           )}
