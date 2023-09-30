3e3e3c752c1eaa1413c70bdfbc99ca8b53e66322
shivamsinghcse <shivamsinghcse21@gmail.com>
2023-09-30 17:57:02 +0530

    routing

--- app/mundalmaster/table.tsx
+++ app/mundalmaster/table.tsx
@@ -36,100 +36,105 @@ export function Table() {
         <button className="px-4 py-2 border-2 mb-5 mx-2 rounded-lg border-gray-400">
           <Link
             className="w-full h-full text-black transition-colors duration-300 hover:bg-gray-100 hover:text-gray-700"
-            href="../sectormasterform"
+            href="../mundalmasterform"
           >
             Add New Mundal
           </Link>
         </button>
       </div>
       <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-  <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-    <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-      <tr>
-        <th scope="col" className="px-6 py-3">
-          Sr.No.
-        </th>
-        <th scope="col" className="px-6 py-3">
-          Mundal 
-        </th>
-        <th scope="col" className="px-6 py-3">
-          Mundal Id 
-        </th>
-        <th scope="col" className="px-6 py-3">
-        Total Sectors
-        </th>
-        <th scope="col" className="px-6 py-3">
-        Total Booths
-        </th>
-        <th scope="col" className="px-6 py-3">
-        Total Karykarta
-        </th>
-        <th scope="col" className="px-6 py-3">
-          Action
-        </th>
-        <th scope="col" className="px-6 py-3">
-          Delete
-        </th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr className="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
-        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-          Apple MacBook Pro
-        </th>
-        <td className="px-6 py-4">
-          Silver
-        </td>
-        <td className="px-6 py-4">
-          Laptop
-        </td>
-        <td className="px-6 py-4">
-          $2999
-        </td>
-        <td className="px-6 py-4">
-          $2999
-        </td>
-        <td className="px-6 py-4">
-          $2999
-        </td>
-        <td className="px-6 py-4">
-          <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-        </td>
-        <td className="px-6 py-4">
-          <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Delete</a>
-        </td>
-      </tr>
-      <tr className="border-b bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
-        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-          Microsoft Surface Pro
-        </th>
-        <td className="px-6 py-4">
-          White
-        </td>
-        <td className="px-6 py-4">
-          Laptop PC
-        </td>
-        <td className="px-6 py-4">
-          $1999
-        </td>
-        <td className="px-6 py-4">
-          $1999
-        </td>
-          <td className="px-6 py-4">
-            $1999
-          </td>
-        <td className="px-6 py-4">
-          <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-        </td>
-        <td className="px-6 py-4">
-          <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Delete</a>
-        </td>
-      </tr>
-      
-    </tbody>
-  </table>
-</div>
-
+        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                Sr.No.
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Mundal Id
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Mundal
+              </th>
+              
+              <th scope="col" className="px-6 py-3">
+                Total Sectors
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Total Booths
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Total Karykarta
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Action
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Delete
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+              <th
+                scope="row"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              >
+                Apple MacBook Pro
+              </th>
+              <td className="px-6 py-4">Silver</td>
+              <td className="px-6 py-4">Laptop</td>
+              <td className="px-6 py-4">$2999</td>
+              <td className="px-6 py-4">$2999</td>
+              <td className="px-6 py-4">$2999</td>
+              <td className="px-6 py-4">
+                <a
+                  href="#"
+                  className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                >
+                  Edit
+                </a>
+              </td>
+              <td className="px-6 py-4">
+                <a
+                  href="#"
+                  className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                >
+                  Delete
+                </a>
+              </td>
+            </tr>
+            <tr className="border-b bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+              <th
+                scope="row"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              >
+                Microsoft Surface Pro
+              </th>
+              <td className="px-6 py-4">White</td>
+              <td className="px-6 py-4">Laptop PC</td>
+              <td className="px-6 py-4">$1999</td>
+              <td className="px-6 py-4">$1999</td>
+              <td className="px-6 py-4">$1999</td>
+              <td className="px-6 py-4">
+                <a
+                  href="#"
+                  className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                >
+                  Edit
+                </a>
+              </td>
+              <td className="px-6 py-4">
+                <a
+                  href="#"
+                  className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                >
+                  Delete
+                </a>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </>
   );
 }
--- app/mundalmasterform/page.tsx
+++ app/mundalmasterform/page.tsx
@@ -13,16 +13,17 @@ function page(){
         </div>
         <div className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
         <form className="w-full max-w-lg">
-            <h1 className="text-2xl">Sector Master Form</h1>
+            <h1 className="text-2xl">Mundal Master Form</h1>
   <div className="flex flex-wrap -mx-3 mb-6 mt-5">
-    <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+    <div className="w-full px-3">
       <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" >
-        Sector Name
+        Mundal Name
       </label>
-      <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Jane" />
+      <input  className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-first-name" type="text" placeholder="Jane" />
       
     </div>
-    <div className="w-full md:w-1/2 px-3">
+    </div>
+    {/* <div className="w-full md:w-1/2 px-3">
       <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" >
         Mundal
       </label>
@@ -44,7 +45,7 @@ function page(){
           <option>Texas</option>
         </select>
     </div>
-    <div className="w-full px-3">
+    <div className="">
       <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" >
       SECTOR PRABHARI
       </label>
@@ -54,7 +55,7 @@ function page(){
           <option>Texas</option>
         </select>
     </div>
-  </div>
+  </div> */}
   
   <button className="appearance-none block w-full bg-orange-600 text-white border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white">Submit</button>
 </form>
