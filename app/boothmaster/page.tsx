2c6e6a52d61525c2dd675b78cfc473c78eabbc93
krishnoit <krishnoit100@gmail.com>
2023-10-01 00:46:52 +0530

    🔥 Remove new format add

--- app/boothmaster/page.tsx
+++ app/boothmaster/page.tsx
@@ -1,8 +1,32 @@
+'use client'
+import { useEffect, useState } from "react";
 import { NavbarLogout } from "../components/navbarlogout";
 import { Sidebar } from "../components/sidebar";
 import { Table } from "./table";
+import { api } from "../pages/api";
 
-function page() {
+function Page() {
+  const [data, setData] = useState({});
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  async function fetchData() {
+    await api
+      .get("/poolingBooth")
+      .then((info) => {
+        setData(info.data.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+      });
+  }
+  useEffect(() => {
+    fetchData();
+  }, []);
+  console.log(data);
+  if (error) return <div>error..... !!!!!</div>;
+  if (loading) return <div>loading....</div>;
   return (
     <>
       <div className="w-[100vw]  z-10">
@@ -20,11 +44,11 @@ function page() {
         <div className="flex justify-center">
         <h1 className="text-2xl font-extrabold mt-10">Booth Master</h1>
         </div>
-          <Table></Table>
+          <Table data={data}></Table>
           </div>
       </div>
      
     </>
   );
 }
-export default page;
+export default Page;
