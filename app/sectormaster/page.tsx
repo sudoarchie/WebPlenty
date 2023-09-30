c1c0f8b0e75ddd21be9319f8d604e28281c5d932
krishnoit <krishnoit100@gmail.com>
2023-09-30 14:16:27 +0530

    ✨ New feature tabel fixed to be done

--- app/sectormaster/page.tsx
+++ app/sectormaster/page.tsx
@@ -2,24 +2,22 @@ import { NavbarLogout } from "../components/navbarlogout";
 import { Sidebar } from "../components/sidebar";
 import { Table } from "./table";
 
-function page(){
-    return (
-        <>
-        <div className="w-[100vw] fixed z-10">
+function page() {
+  return (
+    <>
+      <div className="w-[100vw]  z-10">
         <NavbarLogout />
       </div>
       <div className="flex">
-        <div className="fixed">
+        <div >
           <Sidebar />
         </div>
         <div className="flex justify-center">
-                <h1 className="text-2xl font-extrabold mt-10">
-                कार्यकर्ता विवरण
-                </h1>
-                <Table></Table>
-              </div>
+          <h1 className="text-2xl font-extrabold mt-10">कार्यकर्ता विवरण</h1>
+          <Table></Table>
         </div>
-        </>
-    );
+      </div>
+    </>
+  );
 }
-export default page;    
+export default page;
--- app/sectormaster/table.tsx
+++ app/sectormaster/table.tsx
@@ -1,25 +1,32 @@
 import React from "react";
-import {  Pencil, Trash2 } from "lucide-react";
+import { Pencil, Trash2 } from "lucide-react";
 import Link from "next/link";
 import toast, { Toaster } from "react-hot-toast";
 import { api } from "../pages/api";
 
-function onClickDelete(id: number) {
-    const del = api
-      .delete(`karykarta/${id}`)
-      .then((response) => {
-        toast(response.data.message, {
-          icon: "👏",
-          style: {
-            borderRadius: "10px",
-            background: "#333",
-            color: "#fff",
-          },
-        });
 export function Table() {
-    return (
-        <>
-        <div>
+  // function onClickDelete(id: number) {
+  //   const del = api
+  //     .delete(`karykarta/${id}`)
+  //     .then((response) => {
+  //       toast(response.data.message, {
+  //         icon: "👏",
+  //         style: {
+  //           borderRadius: "10px",
+  //           background: "#333",
+  //           color: "#fff",
+  //         },
+  //       });
+  //     }) // Close the then block here
+  //     .catch((error) => {
+  //       // Handle errors here if needed
+  //       console.error(error);
+  //     });
+  // }
+
+  return (
+    <>
+      <div>
         <button className="px-4 py-2 border-2 mb-5 mx-2 rounded-lg border-gray-400">
           PDF
         </button>
@@ -27,97 +34,125 @@ export function Table() {
           Excel
         </button>
         <button className="px-4 py-2 border-2 mb-5 mx-2 rounded-lg border-gray-400">
-        <Link
-                className="w-full h-full text-black transition-colors duration-300 hover:bg-gray-100 hover:text-gray-700"
-                href="../form"
-              >
-                Add New SectorMaster
-              </Link>
-          
+          <Link
+            className="w-full h-full text-black transition-colors duration-300 hover:bg-gray-100 hover:text-gray-700"
+            href="../form"
+          >
+            Add New SectorMaster
+          </Link>
         </button>
       </div>
-      <table className="min-w-full divide-y divide-gray-800">
-        <thead>
-          <tr className="border-2 border-gray-500">
-          <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-              S.no
-            </th>
-            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-              ID
-            </th>
-            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-              Sector
-            </th>
-            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-              Mandal 
-            </th>
-            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-              Mandal Id
-            </th>
-            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-              
-            </th>
-            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-              Religion
-            </th>
-            
-            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-              Edit
-            </th>
-            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-              Delete
-            </th>
-          </tr>
-          
-        </thead>
-        {data.map((info: any,index:number ) => (
-        <tbody className="bg-white divide-y divide-gray-800">
-          
-            <tr >
-              <td className="px-6 py-4 whitespace-nowrap text-xs"></td>
-              <td className="px-6 py-4 whitespace-nowrap text-xs">
-                
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap text-xs">
-                
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap text-xs">
-                
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap text-xs">
-               
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap text-xs">
-                {/* {info.religion} */}
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap text-xs">
-                {/* {info.gender} */}
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap text-xs">
-                {/* {info.previousParty ? info.previousParty : "None"} */}
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap text-xs">
-                {/* {info.mundal.name} */}
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap text-xs">
-                {/* {info.role} */}
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap border-2 border-red-300">
-                <button>
-                  <Pencil className="h-5 w-5" aria-hidden="true"></Pencil>
-                </button>
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap border-2 border-red-300">
-                <button onClick={() => onClickDelete(info.id)}>
-                  <Trash2 className="h-5 w-5" aria-hidden="true"></Trash2>
-                </button>
-              </td>
-              <Toaster />
-            </tr>
-          
-        </tbody>
-        ))}
-      </table>
-        </>
-    );
+      <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+  <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+    <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+      <tr>
+        <th scope="col" className="px-6 py-3">
+          Product name
+        </th>
+        <th scope="col" className="px-6 py-3">
+          Color
+        </th>
+        <th scope="col" className="px-6 py-3">
+          Category
+        </th>
+        <th scope="col" className="px-6 py-3">
+          Price
+        </th>
+        <th scope="col" className="px-6 py-3">
+          Action
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr className="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+          Apple MacBook Pro 17"
+        </th>
+        <td className="px-6 py-4">
+          Silver
+        </td>
+        <td className="px-6 py-4">
+          Laptop
+        </td>
+        <td className="px-6 py-4">
+          $2999
+        </td>
+        <td className="px-6 py-4">
+          <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+        </td>
+      </tr>
+      <tr className="border-b bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+          Microsoft Surface Pro
+        </th>
+        <td className="px-6 py-4">
+          White
+        </td>
+        <td className="px-6 py-4">
+          Laptop PC
+        </td>
+        <td className="px-6 py-4">
+          $1999
+        </td>
+        <td className="px-6 py-4">
+          <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+        </td>
+      </tr>
+      <tr className="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+          Magic Mouse 2
+        </th>
+        <td className="px-6 py-4">
+          Black
+        </td>
+        <td className="px-6 py-4">
+          Accessories
+        </td>
+        <td className="px-6 py-4">
+          $99
+        </td>
+        <td className="px-6 py-4">
+          <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+        </td>
+      </tr>
+      <tr className="border-b bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+          Google Pixel Phone
+        </th>
+        <td className="px-6 py-4">
+          Gray
+        </td>
+        <td className="px-6 py-4">
+          Phone
+        </td>
+        <td className="px-6 py-4">
+          $799
+        </td>
+        <td className="px-6 py-4">
+          <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+        </td>
+      </tr>
+      <tr>
+        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+          Apple Watch 5
+        </th>
+        <td className="px-6 py-4">
+          Red
+        </td>
+        <td className="px-6 py-4">
+          Wearables
+        </td>
+        <td className="px-6 py-4">
+          $999
+        </td>
+        <td className="px-6 py-4">
+          <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+
+    </>
+  );
 }
