24488a65c2cd952f1e56d481fd66f6a41c97c964
krishnoit <krishnoit100@gmail.com>
2023-10-02 00:37:38 +0530

    🚧 WIP rip the shit

--- app/karykarta/table.tsx
+++ app/karykarta/table.tsx
@@ -1,12 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import { Pencil, Trash2 } from "lucide-react";
 import Link from "next/link";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 import { api } from "../pages/api";
 import toast, { Toaster } from "react-hot-toast";
+import { useForm, SubmitHandler } from "react-hook-form";
+
 export function TableData({ data, url }: { data: any; url: string }) {
   console.log(data);
   console.log(url);
+
+  const [editData, setEditData] = useState<any>(null);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValue>();
+  const onSubmit: SubmitHandler<FormValue> = (data: any) => {
+    postKarykarta(data);
+  };
+
+  async function postKarykarta(data: KarykartaFormData) {
+    return api
+      .post("/karykarta", {
+        ...data,
+      })
+      .then(function (response) {
+        toast(response.data.message, {
+          icon: "👏",
+          style: {
+            borderRadius: "10px",
+            background: "#333",
+            color: "#fff",
+          },
+        });
+      })
+      .catch(function (error) {
+        toast.error(error.response.data.message);
+      });
+  }
+
   function onClickDelete(id: number) {
     const del = api
       .delete(`karykarta/${id}`)
@@ -29,17 +63,20 @@ export function TableData({ data, url }: { data: any; url: string }) {
       });
     console.log(del);
   }
-  function download(type:string) {
-    console.log(type)
+
+  function download(type: string) {
+    console.log(type);
     const apiUrl =
-      url === "/karykarta" ? `${url}?download=true&&type=${type}` : `${url}&&download=true&&type=${type}`;
-  
+      url === "/karykarta"
+        ? `${url}?download=true&&type=${type}`
+        : `${url}&&download=true&&type=${type}`;
+
     api
       .get(apiUrl, { responseType: "blob" })
       .then((response) => {
         const disposition = response.headers["content-disposition"];
-        let filename = `bjp__karykarta__${uuidv4()}`; // Default filename if not found in response headers
-  
+        let filename = `bjp__karykarta__${uuidv4()}`;
+
         if (disposition && disposition.indexOf("attachment") !== -1) {
           const matches = /filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/.exec(
             disposition
@@ -48,139 +85,143 @@ export function TableData({ data, url }: { data: any; url: string }) {
             filename = matches[1].replace(/['"]/g, "");
           }
         }
-  
-        // Create a Blob object and initiate the download
-        const blob = new Blob([response.data], { type: response.headers["content-type"] });
+
+        const blob = new Blob([response.data], {
+          type: response.headers["content-type"],
+        });
         const url = window.URL.createObjectURL(blob);
         const a = document.createElement("a");
         a.href = url;
         a.download = filename;
-        document.body.appendChild(a); // append the element to the DOM
+        document.body.appendChild(a);
         a.click();
-        a.remove(); // afterwards, remove the element
+        a.remove();
       })
       .catch((error) => {
         console.error(error);
       });
   }
-  
+
   return (
     <>
-      <div className="flex justify-center">
-        <button
-          onClick={() => download("pdf")}
-          className="px-4 py-2 border-2 mb-5 mx-2 rounded-lg border-gray-400 text-sm"
-        >
-          PDF
-        </button>
-        <button
-          onClick={() => download("Excel")}
-          className="px-4 py-2 border-2 mb-5 mx-2 rounded-lg border-gray-400 text-sm"
-        >
-          Excel
-        </button>
-        <button className="px-4 py-2 border-2 mb-5 mx-2 rounded-lg border-gray-400">
-          <Link
-            className="w-full h-full text-black transition-colors duration-300 hover:bg-gray-100 hover:text-gray-700 text-sm"
-            href="../form"
-          >
-            Add New Karykarta
-          </Link>
-        </button>
-      </div>
       <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-      <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-          <tr>
-            <th className="px-6 py-3">
-              S.no
-            </th>
-            <th className="px-6 py-3">
-              ID
-            </th>
-            <th className="px-6 py-3">
-              Name
-            </th>
-            <th className="px-6 py-3">
-              Address
-            </th>
-            <th className="px-6 py-3">
-              Mobile Number
-            </th>
-            <th className="px-6 py-3">
-              Date of Birth
-            </th>
-            <th className="px-6 py-3">
-              Religion
-            </th>
-            <th className="px-6 py-3">
-              Gender
-            </th>
-            <th className="px-6 py-3">
-              Previous Party
-            </th>
-            <th className="px-6 py-3">
-              Mundal ID
-            </th>
-            <th className="px-6 py-3">
-              Role
-            </th>
-            <th className="px-6 py-3">
-              Edit
-            </th>
-            <th className="px-6 py-3">
-              Delete
-            </th>
-          </tr>
-        </thead>
-        <tbody >
-          {data.map((info: any, index: number) => (
-            <tr key={info.id} className="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
-              <td className="px-6 py-4">{index}</td>
-              <td className="px-6 py-4">{info.id}</td>
-              <td className="px-6 py-4">
-                {info.name}
-              </td>
-              <td className="px-6 py-4">
-                {info.address}
-              </td>
-              <td className="px-6 py-4">
-                {info.mobileNumber}
-              </td>
-              <td className="px-6 py-4">  
-                {info.dob}
-              </td>
-              <td className="px-6 py-4">
-                {info.religion}
-              </td>
-              <td className="px-6 py-4">
-                {info.gender}
-              </td>
-              <td className="px-6 py-4">
-                {info.previousParty ? info.previousParty : "None"}
-              </td>
-              <td className="px-6 py-4">
-                {info.mundal.name}
-              </td>
-              <td className="px-6 py-4">
-                {info.role}
-              </td>
-              <td className="px-6 py-4 ">
-              <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">
-                <Link href="../karykartaformedit">Edit</Link>
-              </a>
-              </td>
-              <td className="px-6 py-4 ">
-                <button onClick={() => onClickDelete(info.id)} className="font-medium text-blue-600 dark:text-blue-500 hover:underline">
-                  Delete
-                </button>
-              </td>
-              <Toaster />
+        <div className="flex justify-center">
+          <button
+            onClick={() => download("pdf")}
+            className="px-4 py-2 border-2 mb-5 mx-2 rounded-lg border-gray-400 text-sm"
+          >
+            PDF
+          </button>
+          <button
+            onClick={() => download("Excel")}
+            className="px-4 py-2 border-2 mb-5 mx-2 rounded-lg border-gray-400 text-sm"
+          >
+            Excel
+          </button>
+          <button className="px-4 py-2 border-2 mb-5 mx-2 rounded-lg border-gray-400">
+            <Link href="../form">
+              <button className="w-full h-full text-black transition-colors duration-300 hover:bg-gray-100 hover:text-gray-700 text-sm">
+                Add New Karykarta
+              </button>
+            </Link>
+          </button>
+        </div>
+        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th className="px-6 py-3">S.no</th>
+              <th className="px-6 py-3">ID</th>
+              <th className="px-6 py-3">Name</th>
+              <th className="px-6 py-3">Address</th>
+              <th className="px-6 py-3">Mobile Number</th>
+              <th className="px-6 py-3">Date of Birth</th>
+              <th className="px-6 py-3">Religion</th>
+              <th className="px-6 py-3">Gender</th>
+              <th className="px-6 py-3">Previous Party</th>
+              <th className="px-6 py-3">Mundal ID</th>
+              <th className="px-6 py-3">Role</th>
+              <th className="px-6 py-3">Edit</th>
+              <th className="px-6 py-3">Delete</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {data.map((info: any, index: number) => (
+              <tr
+                key={info.id}
+                className="bg-white border-b dark:bg-gray-900 dark:border-gray-700"
+              >
+                <td className="px-6 py-4">{index}</td>
+                <td className="px-6 py-4">{info.id}</td>
+                <td className="px-6 py-4">{info.name}</td>
+                <td className="px-6 py-4">{info.address}</td>
+                <td className="px-6 py-4">{info.mobileNumber}</td>
+                <td className="px-6 py-4">{info.dob}</td>
+                <td className="px-6 py-4">{info.religion}</td>
+                <td className="px-6 py-4">{info.gender}</td>
+                <td className="px-6 py-4">
+                  {info.previousParty ? info.previousParty : "None"}
+                </td>
+                <td className="px-6 py-4">{info.mundal.name}</td>
+                <td className="px-6 py-4">{info.role}</td>
+                <td className="px-6 py-4 ">
+                  {/* Change the anchor to a Link */}
+                  <Link
+  href={{
+    pathname: "../karykartaformedit",
+    query: {
+      data: JSON.stringify(info),
+    },
+  }}
+>
+  <button className="font-medium text-blue-600 dark:text-blue-500 hover:underline">
+    Edit
+  </button>
+</Link>
+                </td>
+                <td className="px-6 py-4 ">
+                  <button
+                    onClick={() => onClickDelete(info.id)}
+                    className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                  >
+                    Delete
+                  </button>
+                </td>
+                <Toaster />
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </>
   );
 }
+
+interface KarykartaFormProps {
+  onSubmit: (formData: KarykartaFormData) => void;
+}
+
+interface KarykartaFormData {
+  name: string;
+  address: string;
+  mobileNumber: string;
+  dob: string;
+  religion: string;
+  gender: string;
+  previousParty?: string;
+  mundalId?: number | null;
+  sectorId?: number | null;
+  poolingBoothId?: number | null;
+  role: string;
+}
+
+type FormValue = {
+  name: string;
+  address: string;
+  mobileNumber: string;
+  dob: string;
+  religion: string;
+  gender: string;
+  previousParty?: string;
+  mundalId: number;
+  role: string;
+};
--- app/karykartaformedit/KarykartaForm.tsx
+++ app/karykartaformedit/KarykartaForm.tsx
@@ -1,8 +1,9 @@
+"use client";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { api, baseURL } from "../pages/api";
-import useSWR from "swr";
 import toast, { Toaster } from "react-hot-toast";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
 interface KarykartaFormProps {
   onSubmit: (formData: KarykartaFormData) => void;
 }
@@ -32,7 +33,7 @@ type FormValue = {
   role: string;
 };
 
-async function postKarykarta(data: KarykartaFormData) {
+async function PostKarykarta(data: KarykartaFormData) {
   return api
     .post("/karykarta", {
       ...data,
@@ -45,9 +46,19 @@ async function postKarykarta(data: KarykartaFormData) {
     });
 }
 
-const KarykartaForm: React.FC<KarykartaFormProps> = () => {
-  const [info, setInfo] = useState(false);
-  const fetcher = (...args: any) => fetch(args).then((res) => res.json());
+const KarykartaForm: React.FC<KarykartaFormProps> = (props) => {
+  const [info, setInfo] = useState<KarykartaData>();
+  const [load, setLoad] = useState(true);
+  const searchParams = useSearchParams();
+  useEffect(() => {
+    const dataParam = searchParams.get("data");
+    if (dataParam) {
+      console.log(dataParam);
+      setInfo(JSON.parse(dataParam));
+      setLoad(false);
+    }
+  }, []);
+
   const {
     register,
     handleSubmit,
@@ -57,8 +68,9 @@ const KarykartaForm: React.FC<KarykartaFormProps> = () => {
     postKarykarta(data);
   };
   async function postKarykarta(data: KarykartaFormData) {
+    console.log(data);
     return api
-      .post("/karykarta", {
+      .put(`/karykarta/${info ? info.id : ""}`, {
         ...data,
       })
       .then(function (response) {
@@ -75,32 +87,7 @@ const KarykartaForm: React.FC<KarykartaFormProps> = () => {
         toast.error(error.response.data.message);
       });
   }
-  const { data, error, isLoading } = useSWR(`${baseURL}/mundal`, fetcher);
-
-  if (error) return <div>failed to load</div>;
-  if (isLoading)
-    return (
-      <div role="status">
-        <svg
-          aria-hidden="true"
-          className="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
-          viewBox="0 0 100 101"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-            fill="currentColor"
-          />
-          <path
-            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-            fill="currentFill"
-          />
-        </svg>
-        <span className="sr-only">Loading...</span>
-      </div>
-    );
-  console.log(data.data[0].name);
+  if (load) return <div> Load...</div>;
 
   return (
     <form
@@ -112,6 +99,7 @@ const KarykartaForm: React.FC<KarykartaFormProps> = () => {
         type="text"
         {...register("name", { required: true })}
         className="w-full p-2 mb-4 border rounded-md"
+        defaultValue={info ? info.name : ""}
       />
 
       <label className="block mb-2 font-bold text-gray-700">Address:</label>
@@ -119,6 +107,7 @@ const KarykartaForm: React.FC<KarykartaFormProps> = () => {
         type="text"
         {...register("address", { required: true })}
         className="w-full p-2 mb-4 border rounded-md"
+        defaultValue={info ? info.address : ""}
       />
 
       <label className="block mb-2 font-bold text-gray-700">
@@ -128,6 +117,7 @@ const KarykartaForm: React.FC<KarykartaFormProps> = () => {
         type="text"
         {...register("mobileNumber", { required: true })}
         className="w-full p-2 mb-4 border rounded-md"
+        defaultValue={info ? info.mobileNumber : ""}
       />
 
       <label className="block mb-2 font-bold text-gray-700">
@@ -137,12 +127,14 @@ const KarykartaForm: React.FC<KarykartaFormProps> = () => {
         type="date"
         {...register("dob", { required: true })}
         className="w-full p-2 mb-4 border rounded-md"
+        defaultValue={info ? info.dob : ""}
       />
 
       <label className="block mb-2 font-bold text-gray-700">Religion:</label>
       <select
         {...register("religion", { required: true })}
         className="w-full p-2 mb-4 border rounded-md"
+        defaultValue={info ? info.religion : ""}
       >
         <option value="hindu">Hindu</option>
         <option value="muslim">Muslim</option>
@@ -156,6 +148,7 @@ const KarykartaForm: React.FC<KarykartaFormProps> = () => {
       <select
         {...register("gender", { required: true })}
         className="w-full p-2 mb-4 border rounded-md"
+        defaultValue={info ? info.gender : ""}
       >
         <option value="Male">Male</option>
         <option value="Female">Female</option>
@@ -168,48 +161,14 @@ const KarykartaForm: React.FC<KarykartaFormProps> = () => {
         type="text"
         {...register("previousParty")}
         className="w-full p-2 mb-4 border rounded-md"
+        defaultValue={info ? info.previousParty : ""}
       />
-
-      <label className="block mb-2 font-bold text-gray-700">Mundal:</label>
-      <select
-        {...register("mundalId", { required: true })}
-        className="w-full p-2 mb-4 border rounded-md"
-      >
-        {!isLoading ? (
-          data.data.map((info: any) => (
-            <option value={info.id} key={info.id}>
-              {info.name}
-            </option>
-          ))
-        ) : (
-          <option value="" key="loading">
-            Loading...
-          </option>
-        )}
-      </select>
-
-      <label className="block mb-2 font-bold text-gray-700">Role:</label>
-      <div className="flex space-x-4">
-        <label className="flex items-center">
-          <select
-            {...register("role", { required: true })}
-            className="w-full p-2 mb-4 border rounded-md"
-          >
-            <option value="karyakarta">कार्यकर्ता</option>
-            <option value="adhyaksha">अध्यक्ष</option>
-            <option value="upaadhyaksha">उपाध्यक्ष</option>
-            <option value="mahamantri">महामन्त्री</option>
-            <option value="mantri">मंत्री</option>
-            <option value="koshadhyaksha">कोषाध्यक्ष</option>
-          </select>
-        </label>
-      </div>
 
       <button
         type="submit"
         className="mt-4 p-2 bg-orange-600 text-white rounded-md hover:bg-orange-700 focus:outline-none focus:shadow-outline-blue active:bg-blue-800"
       >
-        Submit
+        Edit
       </button>
       <Toaster />
     </form>
@@ -217,3 +176,28 @@ const KarykartaForm: React.FC<KarykartaFormProps> = () => {
 };
 
 export default KarykartaForm;
+
+interface KarykartaData {
+  id: number;
+  name: string;
+  address: string;
+  mobileNumber: string;
+  dob: string;
+  religion: string;
+  gender: string;
+  previousParty: string;
+  mundalId: number;
+  sectorId: number;
+  poolingBoothid: number | null;
+  role: string;
+  mundal: {
+    id: number;
+    name: string;
+  };
+  sector: {
+    id: number;
+    name: string;
+    mundalId: number;
+  };
+  poolingBooth: null; // Assuming 'poolingBooth' is always null in this data
+}
