cbb0f6c88743097605ec8b414111c7f5c223ed7c
krishnoit <krishnoit100@gmail.com>
2023-10-02 03:34:43 +0530

    🔥 Remove new api route changed

--- app/pages/api/index.ts
+++ app/pages/api/index.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-export const baseURL = "http://localhost:5000/api"
+export const baseURL = "http://13.232.29.234:5000/api"
 export const api = axios.create({
     baseURL,
     headers: {
