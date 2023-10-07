b6f84ed9d9ba8eeafb5f9168341fcb581ee319a0
krishnoit <krishnoit100@gmail.com>
2023-10-07 10:41:10 +0530

    ✨ New feature new

--- app/pages/api/index.ts
+++ app/pages/api/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-export const baseURL = "http://localhost:5000/api"; 
-
+// export const baseURL = "http://localhost:5000/api"; 
+export const baseURL = "http://3.108.17.52:5000/api"; 
 export const api = axios.create({
   baseURL,
   headers: {
