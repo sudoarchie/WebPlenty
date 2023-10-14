6bab3b7f4ae878109b28bd34f6d9d237210181ec
shivamsinghcse <shivamsinghcse21@gmail.com>
2023-10-14 19:35:48 +0530

    changing the error

--- app/page.tsx
+++ app/page.tsx
@@ -13,9 +13,9 @@ export default function Home() {
       <Navbar></Navbar>
       <Hero></Hero>
       <Leadabout></Leadabout>
-      <div className="flex justify-center">
-        {/* <h2 className="font-bold text-3xl underline">हमारा कार्य</h2> */}
-      </div>
+      {/* <div className="flex justify-center">
+         <h2 className="font-bold text-3xl underline">हमारा कार्य</h2> 
+      </div> */}
       {/* <Writting></Writting>
       <div className="flex justify-center">
         <h2 className="font-bold text-3xl underline">Gallery</h2>
